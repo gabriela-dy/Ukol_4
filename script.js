13983e6c9616b15ca24440ac55f90496e9b50ebb
Gabriela Dyworova <gabriela.dyworova@gmail.com>
2024-03-06 13:35:52 +0100

    ukol-4 funkční

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let cislo1 = Number(prompt("Zadej první číslo"));
+let cislo2 = Number(prompt("Zadej druhé číslo"));
+let cislo3 = Number(prompt("Zadej třetí číslo"));
+
+function vratMaxCislo(cislo1, cislo2, cislo3) {
+    if (cislo1 >= cislo2 && cislo1 >= cislo3) {
+        return cislo1;
+    } else if (cislo2 >= cislo1 && cislo2 >= cislo3) {
+        return cislo2;
+    } else {
+        return cislo3;
+    }
+}
+
+let maxCislo = vratMaxCislo(cislo1, cislo2, cislo3);
+console.log("Největší číslo je: " + maxCislo);
